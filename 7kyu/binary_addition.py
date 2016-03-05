dddc29c2182f490b0a5f6edd94c6693502ce75ce
Paige Bolduc <paigebolduc@gmail.com>
2016-03-05 11:36:25 -0800

    add solution for sum the array

--- 7kyu/binary_addition.py
+++ 7kyu/binary_addition.py
@@ -3,5 +3,15 @@
 # addition of the two.
 # The binary number returned should be a string!
 
-def add_binary(a,b):
-    return bin(a+b)[2:]
+def add_binary(a, b):
+    return bin(a + b)[2:]
+
+# alternatively:
+
+def add_binary(a, b):
+    sum = a + b
+    binary_string = ""
+    while sum > 0:
+        binary_string += str(sum % 2)
+        sum = sum / 2
+    return binary_string[::-1]
---
+++ 7kyu/sumTheArray.js
@@ -0,0 +1,22 @@
+/* Your job is to add a method 'sum' to all array objects, which sums all the
+numbers in it. You should return 0 if the array is empty.
+
+For example:
+var arr = [1,2,3,4];
+arr.sum();  //returns 10 */
+
+
+Array.prototype.sum = function() {
+ var sum = 0;
+
+ for (var i = 0; i < this.length; i++) {
+   sum += this[i]
+   }
+ return sum;
+}
+
+// alternatively:
+
+Array.prototype.sum = function() {
+  return this.reduce((a, b) => a + b, 0);
+};
