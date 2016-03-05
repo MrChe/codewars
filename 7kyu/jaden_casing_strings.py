c7ed8067a80b74edb14a7a5a99bc31b377613719
Paige Bolduc <paigebolduc@gmail.com>
2016-03-05 14:20:35 -0800

    add js solution for jaden casing strings challenge

--- 7kyu/jaden_casing_strings.py
+++ 7kyu/jaden_casing_strings.py
@@ -1,3 +1,11 @@
+""" Jaden Smith, the son of Will Smith, is the star of films such as The Karate
+Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy
+that he delivers via Twitter. When writing on Twitter, he is known for almost
+always capitalizing every word.
+
+Your task is to convert strings to how they would be written by Jaden Smith.
+The strings are actual quotes from Jaden Smith, but they are not capitalized in
+the same way he originally typed them."""
 
 
 def toJadenCase(string):
@@ -15,4 +23,3 @@ def toJadenCase(string):
 def toJadenCase(string):
   capitalized_words = [word.capitalize() for word in string.split(" ")]
   return " ".join(capitalized_words)
-  
